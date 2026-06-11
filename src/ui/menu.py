63e07e027c0a4725f1f673d3430d60cb6196abe3
yzigui <yasmimguimaraes883@gmail.com>
2026-06-11 11:27:22 +0100

    funcionalidade adicionada
    Implementada a função " adicionar projeto"

--- src/ui/menu.py
+++ src/ui/menu.py
@@ -21,5 +21,49 @@ class Menu:
                 print("Programa encerrado.")
                 break
 
+            elif opcao == "1":
+                self.adicionar_projeto()
+
             else:
                 print("Funcionalidade ainda não implementada.")
+
+    def adicionar_projeto(self):
+        try:
+            nome = input("Nome do projeto: ")
+
+            print("\nCategorias:")
+            categorias = self.bll.listar_categorias()
+
+            for i, categoria in enumerate(categorias, start=1):
+                print(f"{i} - {categoria}")
+
+            opcao_categoria = int(input("Escolha a categoria: "))
+            categoria = categorias[opcao_categoria - 1]
+
+            descricao = input("Descrição do projeto: ")
+            ferramentas = input("Ferramentas utilizadas: ")
+            data_inicio = input("Data de início: ")
+            concluido = input("O projeto foi concluído? (S/N): ")
+
+            data_conclusao = ""
+
+            if concluido.upper() == "S":
+                data_conclusao = input("Data de conclusão: ")
+
+            projeto = self.bll.adicionar_projeto(
+                nome,
+                categoria,
+                descricao,
+                ferramentas,
+                data_inicio,
+                concluido,
+                data_conclusao
+            )
+
+            print(f"\nProjeto '{projeto['nome']}' adicionado com sucesso!")
+
+        except ValueError as erro:
+            print(f"\nErro: {erro}")
+
+        except IndexError:
+            print("\nErro: categoria inválida.")
